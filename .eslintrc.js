1782462f1d3146525853633b8f1e30ea61b3e3fc
Sam Hauglustaine <sam.hauglustaine@gmail.com>
2019-12-31 18:16:21 +0100

    rerun eslint init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
   },
-  'extends': 'standard',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 2018
   },
-  'rules': {
-    'semi': ['error', 'always']
+  rules: {
+    semi: ['error', 'always']
   }
 }
